7a7502af455993af33a98a1a90c2ff4ec04c263e
Travis CI User <travis@example.org>
2018-03-20 11:19:56 +0000

    Update docs after building Travis build 756 of bashtage/ng-numpy-randomstate
    The docs were built from the branch 'master' against the commit
    178708f4489df55db0f06fbcc2394335ca3f7a20.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bashtage/ng-numpy-randomstate/jobs/355796083.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/randomstate-test/bin/doctr deploy doc

--- doc/_static/documentation_options.js
+++ doc/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '1.13.3+6.gd378c12',
+    VERSION: '1.13.3+7.g178708f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
